ffdb75d36a4dce617b5abb05ed95e7dcbaf29068
norrie-adams <normanadams087@gmail.com>
2026-01-26 16:00:31 -0500

    Pulled API data from OpenWeatherMap
    1. Connected to OpenWeatherMap API
    2. Allowed user input for a city
    3. Printed temperature, feels like, and current conditions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import requests
+
+base_url = f"http://api.openweathermap.org/data/2.5/weather?"
+API_key = "12407454b28300b217a183936ecfe7e1"
+city = input("Enter a City: ")
+
+url = base_url + "appid=" + API_key + "&q=" + city
+
+data = requests.get(url).json()
+
+temp = data["main"]["temp"]
+feels_like = data["main"]["feels_like"]
+description = data["weather"][0]["description"]
+
+print(f"The temperature is {temp}, it feels like {feels_like}, and it is {description}")
