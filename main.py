ea32b50ff9c21f2b9e0c3a76e7a427d04be4b1e1
norrie-adams <normanadams087@gmail.com>
2026-01-27 11:56:45 -0500

    Invalid City Error
    1. Prints invalid city message if the city is invalid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+#Used to declare a city invalid
+invalid = False
+
 #API Key
 API_key = "12407454b28300b217a183936ecfe7e1"
 
@@ -10,16 +13,31 @@ def get_weather(city):
 
     data = requests.get(url).json()
 
-    temps = data["main"]["temp"]
-    feels_like = data["main"]["feels_like"]
-    description = data["weather"][0]["description"]
+    #Checks for invalid cities
+    if "main" not in data:
+        invalid_city()
+    
+    #Pulls data from API
+    else:
+        temps = data["main"]["temp"]
+        feels_like = data["main"]["feels_like"]
+        description = data["weather"][0]["description"]
 
-    return temps, feels_like, description
+        return temps, feels_like, description
 
 #Displays data from API
 def provide_data(temps, feels_like, description):
+    print(f"The temperature is {temps}, it feels like {feels_like}, and it is {description}")
+
+#Prints invalid city message
+def invalid_city():
+    print("Please enter in a valid city")
+    invalid = True
+
 #Asks for user input
+city = input("Enter a City: ")
 
 #Actually displays data
-temps, feels_like, description = get_weather(city)
-provide_data(temps, feels_like, description)
+if invalid is not True:
+    temps, feels_like, description = get_weather(city)
+    provide_data(temps, feels_like, description)
